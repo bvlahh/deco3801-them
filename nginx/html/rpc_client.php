8edb26e999ad7931f70b4017f4d8259e15bc5adc
Brendan Miller <s4205226@gs109-1280.labs.eait.uq.edu.au>
2013-08-19 16:56:39 +1000

    Added rpc call indices

--- nginx/html/rpc_client.php
+++ nginx/html/rpc_client.php
@@ -6,7 +6,12 @@ function rpc_validate($files, $document) {
 	
 	$parser = new JsonRpcClient("http://localhost:8080");
 	
-	return $parser->parse_html( array($files, $document) );
+	return $parser->parse_html(
+			array(
+					"files" => $files,
+					"document" => $document
+				)
+		);
 	
 }
 
