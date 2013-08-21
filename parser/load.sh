e98be57b1a9ecf6e17db054e89d35d53aef4d355
bvlahh <brendan.miller@uqconnect.edu.au>
2013-08-22 02:39:30 +1000

    Fixed the load script

--- parser/load.sh
+++ parser/load.sh
@@ -17,16 +17,16 @@ tar -cf temp1.tar temp1/;
 
 rm -rf ./temp1;
 
-scp ./temp.tar1 $server_user@$server_address:~;
+scp ./temp1.tar $server_user@$server_address:~;
 
-rm -f ./temp.tar1;
+rm -f ./temp1.tar;
 
 ssh $server_user@$server_address "
 
 rm -rf $app_dir;
 
-tar -xmf temp.tar1;
-mv -f ./temp/ $app_dir;
+tar -xmf temp1.tar;
+mv -f ./temp1/ $app_dir;
 
 rm -rf ./temp1;
 rm -f ./temp1.tar;
