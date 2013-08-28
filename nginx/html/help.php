00bb9458a005a8dd586731ec9acf4774a558b964
bvlahh <brendan.miller@uqconnect.edu.au>
2013-08-28 12:14:28 +1000

    Removed 'yay' from errorbar, moved demo error bar to help page

--- nginx/html/help.php
+++ nginx/html/help.php
@@ -2,6 +2,7 @@
 
 require_once "php/header.php";
 require_once "php/footer.php";
+require_once "php/errorbar.php";
 
 draw_header("THEM prototype - Help", 4);
 
@@ -10,6 +11,17 @@ print <<<END
     Help Section
     
 END;
+
+print <<<END
+    
+    Demo Error Bars:
+    
+END;
+
+draw_error_bar(1, 1, 1, 1, 1, 500, 10);
+draw_error_bar(1, 1, 1, 1, 1, 200, 10);
+draw_error_bar(1, 1, 1, 1, 1, 500, 10, true);
+draw_error_bar(1, 1, 1, 1, 1, 200, 10, true);
 
 draw_footer();
 
--- nginx/html/php/errorbar.php
+++ nginx/html/php/errorbar.php
@@ -34,13 +34,13 @@ function draw_error_bar($blue, $red, $orange, $yellow, $green, $total_wide, $tot
         
         if ($green == 0)
             $green = "";
-        else
-            $green = "YAY";
+        //else
+        //    $green = "YAY";
         
     }
     
     print <<<END
-        <div style="width: ${total_wide}px; border: 1px solid #DDDDDD;">
+        <div style="width: ${total_wide}px; border: 1px solid #DDDDDD; margin: 10px;">
             <div style="float: left; width: ${blue_wide}px; background-color: #7f7fff; text-align: center; padding: 10px 0px 10px 0px;">${blue}</div>
             <div style="float: left; width: ${red_wide}px; background-color: #ff7f7f; text-align: center; padding: 10px 0px 10px 0px;">${red}</div>
             <div style="float: left; width: ${orange_wide}px; background-color: #7f7f7f; text-align: center; padding: 10px 0px 10px 0px;">${orange}</div>
