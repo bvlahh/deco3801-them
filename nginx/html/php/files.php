f08be1647fdeb021e969101e67cc210ba653fb1c
bvlahh <brendan.miller@uqconnect.edu.au>
2013-09-01 19:58:40 +1000

    Separated db config out of db functions

--- nginx/html/php/files.php
+++ nginx/html/php/files.php
@@ -1,13 +1,12 @@
 <?php
 
-//$res = $mysqli->query("SELECT NOW();");
-//while ( $row = $res->fetch_assoc() )
-//    print_r( $row );
+define("DB_HOST", "localhost");
+define("DB_NAME", "validator");
 
 function create_set($text) {
     
-    $mysqli = new mysqli("localhost");
-    $mysqli->select_db("validator");
+    $mysqli = new mysqli(DB_HOST);
+    $mysqli->select_db(DB_NAME);
     $text = $mysqli->real_escape_string($text);
     $res = $mysqli->query("call create_set(\"$text\");");
     $row = $res->fetch_array();
@@ -17,8 +16,8 @@ function create_set($text) {
 
 function add_file($upload_set, $filename, $document, $parse_data) {
     
-    $mysqli = new mysqli("localhost");
-    $mysqli->select_db("validator");
+    $mysqli = new mysqli(DB_HOST);
+    $mysqli->select_db(DB_NAME);
     $upload_set = (int)$upload_set;
     $filename = $mysqli->real_escape_string($filename);
     $document = $mysqli->real_escape_string($document);
@@ -33,8 +32,8 @@ function add_file($upload_set, $filename, $document, $parse_data) {
 
 function cache_parse($file_id, $parse_data) {
     
-    $mysqli = new mysqli("localhost");
-    $mysqli->select_db("validator");
+    $mysqli = new mysqli(DB_HOST);
+    $mysqli->select_db(DB_NAME);
     $file_id = (int)$file_id;
     $parse_data = $mysqli->real_escape_string($parse_data);
     $res = $mysqli->query("call cache_parse($file_id, \"$parse_data\");");
@@ -43,8 +42,8 @@ function cache_parse($file_id, $parse_data) {
 
 function get_file($file_id) {
     
-    $mysqli = new mysqli("localhost");
-    $mysqli->select_db("validator");
+    $mysqli = new mysqli(DB_HOST);
+    $mysqli->select_db(DB_NAME);
     $file_id = $mysqli->real_escape_string($file_id);
     $res = $mysqli->query("call get_file(\"$file_id\");");
     $row = $res->fetch_assoc();
