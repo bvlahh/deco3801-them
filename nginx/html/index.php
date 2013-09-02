1b0b6f4141cf54f7d6f8c899041310d489d692c4
bvlahh <brendan.miller@uqconnect.edu.au>
2013-09-02 17:16:47 +1000

    added padding to index

--- nginx/html/index.php
+++ nginx/html/index.php
@@ -13,16 +13,20 @@ print <<<END
             Welcome to The HTML5 Evaluation Machine
         </div>
 
-        <div>This web application allows you to directly input or upload HTML files and entire websites. These files are evaluated for their adherance to best style practices in four major categories:<br /><br />
+        <div style="padding: 10px;">
+        
+        This web application allows you to directly input or upload HTML files and entire websites. These files are evaluated for their adherance to best style practices in four major categories:<br /><br />
         
         <div style="padding-left: 10px">Syntax / Semantics<br />
         Deprecated Elements<br />
         Accessibility<br />
         Poor Practice / Other Miscellenous Issues.<br /><br /></div>
 
-        Start uploading now by clicking on <a href="/direct_input">Direct Input<a>, <a href="/upload_file">Upload File</a> or <a href="/upload_zip">Upload Zip</a> in the sidebar.<br \><br \>
+        Start uploading now by clicking on <a href="/direct_input">Direct Input</a>, <a href="/upload_file">Upload File</a> or <a href="/upload_zip">Upload Zip</a> in the sidebar.<br \><br \>
 
-        For more information on how everything works, check out the <a href="/help">help page</a>!</div>
+        For more information on how everything works, check out the <a href="/help">help page</a>!
+        
+        </div>
         
         <div class="cb"></div>
         
