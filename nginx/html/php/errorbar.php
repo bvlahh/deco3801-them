ce7ec9153027a7e0dec68e4a64a2659216091005
bvlahh <brendan.miller@uqconnect.edu.au>
2013-09-02 17:34:07 +1000

    Made the height work

--- nginx/html/php/errorbar.php
+++ nginx/html/php/errorbar.php
@@ -48,18 +48,16 @@ function draw_error_bar($blue, $red, $orange, $yellow, $green, $total_wide, $tot
         
         if ($green == 0)
             $green = "";
-        //else
-        //    $green = "YAY";
         
     }
     
     print <<<END
         <div style="width: ${total_wide}px; border: 1px solid #DDDDDD; margin: 10px;">
-            <div style="float: left; width: ${blue_wide}px; background-color: #7f7fff; text-align: center; padding: 10px 0px 10px 0px;">${blue}</div>
-            <div style="float: left; width: ${red_wide}px; background-color: #ff7f7f; text-align: center; padding: 10px 0px 10px 0px;">${red}</div>
-            <div style="float: left; width: ${orange_wide}px; background-color: #ffbb77; text-align: center; padding: 10px 0px 10px 0px;">${orange}</div>
-            <div style="float: left; width: ${yellow_wide}px; background-color: #ffff7f; text-align: center; padding: 10px 0px 10px 0px;">${yellow}</div>
-            <div style="float: left; width: ${green_wide}px; background-color: #7fff7f; text-align: center; padding: 10px 0px 10px 0px;">${green}</div>
+            <div style="float: left; width: ${blue_wide}px; background-color: #7f7fff; text-align: center; height: ${total_height}px; font-size: ${total_height}px;">${blue}</div>
+            <div style="float: left; width: ${red_wide}px; background-color: #ff7f7f; text-align: center; height: ${total_height}px; font-size: ${total_height}px;">${red}</div>
+            <div style="float: left; width: ${orange_wide}px; background-color: #ffbb77; text-align: center; height: ${total_height}px; font-size: ${total_height}px;">${orange}</div>
+            <div style="float: left; width: ${yellow_wide}px; background-color: #ffff7f; text-align: center; height: ${total_height}px; font-size: ${total_height}px;">${yellow}</div>
+            <div style="float: left; width: ${green_wide}px; background-color: #7fff7f; text-align: center; height: ${total_height}px; font-size: ${total_height}px;">${green}</div>
             <div class="cb"></div>
         </div>
 END;
