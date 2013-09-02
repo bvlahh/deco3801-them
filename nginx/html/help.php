1aedddcef04f645b43dd7f0bb7dc94971ae1b913
Scott Heiner <veritasdragon@hotmail.com>
2013-09-02 17:38:31 +1000

    some work done on help
    i did some stuff with help file. also added a file for me.

--- nginx/html/help.php
+++ nginx/html/help.php
@@ -16,15 +16,32 @@ END;
 
 print <<<END
     
-    <br />
-    <br />
-    
-    Error Bars - Any size you want:
+    Understanding the Error Bars:<br />
+
+    Here you can see an error bar, with corresponding error numbers included.<br />
     
 END;
 
-draw_error_bar(1, 1, 1, 1, 1, 500, 10);
-draw_error_bar(1, 1, 1, 1, 1, 500, 10, true);
+draw_error_bar(1, 6, 3, 2, 0, 500, 10);
+
+print <<<END
+
+	<br />This bar shows the relative ratios of each error in a document. In order, this highlights:<br /><br />
+
+	put the types here in order.<br />
+
+	If the file has no errors, a green bar will display instead.<br />
+
+END;
+
+draw_error_bar(0, 0, 0, 0, 1, 500, 10);
+
+print <<<END
+
+	more text here.
+
+END;
+
 draw_error_bar(1, 1, 1, 1, 1, 250, 10);
 draw_error_bar(1, 1, 1, 1, 1, 250, 10, true);
 
