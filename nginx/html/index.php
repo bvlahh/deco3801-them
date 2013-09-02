dfbfe989582aa3b41558fbfb675897ad9fd5eeb0
Scott Heiner <veritasdragon@hotmail.com>
2013-09-02 16:36:28 +1000

    Edits to index and style
    Now includes some information about the tool and directs users to help
    to find out more.

--- nginx/html/index.php
+++ nginx/html/index.php
@@ -7,29 +7,22 @@ draw_header("THEM prototype", 0);
 
 print <<<END
     
-    <div class="startbuttons">
+    <div style="width: 500px">
         
-        <div>
+        <div class="indextitle">
             Welcome to The HTML5 Evaluation Machine
         </div>
+
+        <div>This web application allows you to directly input or upload HTML files and entire websites. These files are evaluated for their adherance to best style practices in four major categories:<br /><br />
         
-        <a href="/direct_input" >
-            Direct Input
-        </a>
-        
-        <a href="/upload_file" >
-            Upload File
-        </a>
-        
-        <br />
-        
-        <a href="/upload_zip" >
-            Upload ZIP
-        </a>
-        
-        <a href="/help" >
-            Help
-        </a>
+        Syntax / Semantics<br />
+        Deprecated Elements<br />
+        Accessibility<br />
+        Poor Practice / Other Miscellenous Issues.<br /><br />
+
+        Start uploading now by clicking on <a href="/direct_input">Direct Input<a>, <a href="/upload_file">Upload File</a> or <a href="/upload_zip">Upload Zip</a> in the sidebar.<br \><br \>
+
+        For more information on how everything works, check out the <a href="/help">help page</a>!</div>
         
         <div class="cb"></div>
         
