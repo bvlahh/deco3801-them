e716665854722660846b678b6dc0057a03f7102f
bvlahh <brendan.miller@uqconnect.edu.au>
2013-10-09 15:05:39 +1000

    Changed the uploaded file display

--- nginx/html/show_set.php
+++ nginx/html/show_set.php
@@ -17,27 +17,97 @@ $set = get_set($set);
 if ( count($set) < 2 )
     redirect("/show_file?file=" . $set[0]["id"]);
 
+$filetree = array(
+    "files" => array(),
+    "folders" => array()
+);
+
+foreach ( $set as $file ) {
+    
+    $filename = $file["filename"];
+    
+    $filepath = explode("/", $filename);
+    
+    // start at the root
+    $filetreeparent = &$filetree;
+    
+    // loop over the folders leading to the file
+    for ($f=0; $f<(count($filepath) - 1); $f++) {
+        
+        // if the folder doesn't exist in the tree, make it.
+        if (! array_key_exists($filepath[$f], $filetreeparent["folders"]) )
+            $filetreeparent["folders"][$filepath[$f]] = array(
+                "files" => array(),
+                "folders" => array()
+            );
+        
+        // set that as the current parent
+        $filetreeparent = &$filetreeparent["folders"][$filepath[$f]];
+        
+    }
+    
+    // if it's a file put it in the files array
+    if ( $filepath[ count($filepath) - 1 ] != "" )
+        $filetreeparent["files"][ $filepath[count($filepath) - 1] ] = $file;
+    
+}
+
+function draw_file($file) {
+    
+    $filename = $file["filename"];
+    
+    $filename = preg_replace("/.*\\//", "", $filename);
+    
+    $fid = $file["id"];
+    
+    print <<<END
+        <a class="files_file" href="/show_file?file=${fid}">
+            <img src="/images/white_file.png" alt="" />
+            <br />
+            ${filename}
+        </a>
+END;
+    
+}
+
+function draw_folder($folder_name, $folder) {
+    
+    $escaped_folder_name = htmlspecialchars($folder_name);
+    
+    print <<<END
+        <div class="files_folder">
+            <div class="files_folder_title">
+                ${escaped_folder_name}
+            </div>
+            <div class="files_folder_contents">
+                <div>
+END;
+    
+    foreach ( $folder["folders"] as $subfolder_name => $subfolder)
+        draw_folder($subfolder_name, $subfolder);
+    
+    print <<<END
+            </div>
+            <div class="cb"></div>
+            <div>
+END;
+    
+    foreach ( $folder["files"] as $display_name => $file)
+        draw_file($file);
+    
+    print <<<END
+                </div>
+                <div class="cb"></div>
+            </div>
+        </div>
+END;
+    
+}
+
 draw_header("THEM prototype - Uploaded Files");
 
-foreach( $set as $file ) {
-    
-    //${file["cached_parse"]}
-    
-    print <<<END
-        <a href="/show_file?file=${file["id"]}">${file["filename"]}</a>
-END;
-
-	$parsed = json_decode($file["cached_parse"]);
-
-	if ( count($parsed) == 0 )
-	    draw_error_bar(0, 0, 0, 0, 1, 500, 10);
-	else
-	    draw_error_bar(0, 1, 0, 0, 0, 500, 10);
-    
-    print <<<END
-    	<br />
-END;
-}
+// Uploaded Files or Uploaded Zip if the files[] != []
+draw_folder("Uploaded Files", $filetree);
 
 draw_footer();
 
