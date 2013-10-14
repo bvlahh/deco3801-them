7872638f615bd2dadbf06420c4b1d61d1d082f20
veritasunae <veritasdragon@hotmail.com>
2013-10-14 15:25:32 +1000

    User directive changed
    Shifted where we put the directive to users to click on highlighted
    text.

--- nginx/html/show_file.php
+++ nginx/html/show_file.php
@@ -259,9 +259,9 @@ print <<<END
 
 </div>
 
-<div>Click on highlighted text for more information.</div>
-
 <div class="top_infobox" id="top_infobox">$top_info</div>
+
+<div style="padding: 10px;">Click on highlighted text for more information.</div>
 
 <div class="file" style="float: left; margin-top: 10px">
     
