8e26a71a95a21261964dcc94fb123e21de8c42aa
Brendan Miller <s4205226@gs109-1264.labs.eait.uq.edu.au>
2013-10-16 18:37:34 +1000

    added bars to uploaded set page

--- nginx/html/php/errorbar.php
+++ nginx/html/php/errorbar.php
@@ -52,14 +52,14 @@ function draw_error_bar($blue, $red, $orange, $yellow, $green, $total_wide, $tot
     }
     
     print <<<END
-        <div style="width: ${total_wide}px; border: 1px solid #DDDDDD;">
-            <div style="float: left; width: ${blue_wide}px; background-color: #7f7fff; text-align: center; height: ${total_height}px; font-size: ${total_height}px;">${blue}</div>
-            <div style="float: left; width: ${red_wide}px; background-color: #ff7f7f; text-align: center; height: ${total_height}px; font-size: ${total_height}px;">${red}</div>
-            <div style="float: left; width: ${orange_wide}px; background-color: #ffbb77; text-align: center; height: ${total_height}px; font-size: ${total_height}px;">${orange}</div>
-            <div style="float: left; width: ${yellow_wide}px; background-color: #ffff7f; text-align: center; height: ${total_height}px; font-size: ${total_height}px;">${yellow}</div>
-            <div style="float: left; width: ${green_wide}px; background-color: #7fff7f; text-align: center; height: ${total_height}px; font-size: ${total_height}px;">${green}</div>
-            <div class="cb"></div>
-        </div>
+        <span style="display: block; width: ${total_wide}px; border: 1px solid #DDDDDD; margin: auto auto;">
+            <span style="display: block; float: left; width: ${blue_wide}px; background-color: #7f7fff; text-align: center; height: ${total_height}px; font-size: ${total_height}px;">${blue}</span>
+            <span style="display: block; float: left; width: ${red_wide}px; background-color: #ff7f7f; text-align: center; height: ${total_height}px; font-size: ${total_height}px;">${red}</span>
+            <span style="display: block; float: left; width: ${orange_wide}px; background-color: #ffbb77; text-align: center; height: ${total_height}px; font-size: ${total_height}px;">${orange}</span>
+            <span style="display: block; float: left; width: ${yellow_wide}px; background-color: #ffff7f; text-align: center; height: ${total_height}px; font-size: ${total_height}px;">${yellow}</span>
+            <span style="display: block; float: left; width: ${green_wide}px; background-color: #7fff7f; text-align: center; height: ${total_height}px; font-size: ${total_height}px;">${green}</span>
+            <span style="display: block;" class="cb"></span>
+        </span>
 END;
     
 }
