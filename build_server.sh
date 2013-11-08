1eaf68405a55670c2330af5ae67a94b567263d47
Brendan Miller <Brendan@Cupertino.local>
2013-11-08 10:04:48 +1000

    Updated supervisord config

--- build_server.sh
+++ build_server.sh
@@ -10,8 +10,14 @@ scp -r ./config $1@$2:~
 ssh $1@$2 '
 cd config
 ./server_setup.sh
-';
+'
 
-#scp -r ./nginx $USER@$SERVER:/
-#scp -r ./parser $USER@$SERVER:/
-#scp -r ./config $USER@$SERVER:/
+cd nginx
+./load.sh $1 $2
+
+cd ..
+
+cd parser
+./load.sh $1 $2
+
+cd ..
